4f5b14d947eda1896fbd0ee2b02cb019eb000b3e
Ahmed Fetni <ahmedfetni277@gmail.com>
2023-08-17 19:23:02 +0100

    just an empty component for the list of comments

--- src/QuestionCompopnents/ListOfComment.tsx
+++ src/QuestionCompopnents/ListOfComment.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 function ListOfComment() {
   return (
     <div>
-      
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   )
 }
